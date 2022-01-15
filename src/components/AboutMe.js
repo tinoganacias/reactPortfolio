ecbd903e75a040f3fe924f46d2c49bf54ac6bae6
Tino Ganacias <84544540+tinoganacias@users.noreply.github.com>
2022-01-15 13:49:10 -0800

    Update AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,7 @@ const AboutMe = () => {
     return (
         <div className="jumbotron jumbotron-fluid" id="jumbotron">
         <div className="container" >
-            <img className="profile-pic profile" src="https://drive.google.com/file/d/1dx_9VWPHxrxSsoVRbTSwmvDve56g4dKT/view" alt="pic of tino"/> 
+            <img className="profile-pic profile" src="https://github.com/tinoganacias/reactPortfolio/blob/main/src/assets/blueSweater.png?raw=true" alt="pic of tino"/> 
             <h1 className="name" id="name"><strong>Tino Ganacias</strong></h1>
             <p className="lead" id="portfolio">After earning my Bachelors of Arts degree from the University of Michigan, I entered the world of restaurants and hospitality exploring my passion for service, creativity, and entrepeneurialism.  I've realized my heart and my brain are rooted in patterns, logic, and coding.  I am focused and excited to enter the world of computer technology and computer programming, with a foundation of building successful businesses and people management.</p>
         </div>
